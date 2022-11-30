8458840e5547e8b548f0f06436e0343c534fc9f0
nico vscode <devantherynicolas@gmail.com>
2022-11-30 16:59:46 +0100

    route tracing and gridview on search page

--- lib/Screens/map/mapView.dart
+++ lib/Screens/map/mapView.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:http/http.dart' as http;
 
 class MapPage extends StatefulWidget {
-  const MapPage({Key? key}) : super(key: key);
+  List<LatLng>? points;
+  MapPage({Key? key, this.points}) : super(key: key);
 
   @override
   State<MapPage> createState() => _MapPageState();
@@ -12,6 +15,9 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   Position? _position;
+  late LatLng _clicked;
+  bool switchValue = false;
+  List<LatLng> myRoute = [];
 
   void _getCurrentLocation() async {
     Position position = await _determinePosition();
@@ -45,73 +51,156 @@ class _MapPageState extends State<MapPage> {
 
   @override
   void initState() {
+    _clicked = LatLng(0, 0);
+    widget.points = [
+      LatLng(46.224410, 7.355230),
+      LatLng(46.254379, 6.951690),
+    ];
+
+    if (widget.points != null) {
+      _getRoute(widget.points!).then((value) => setState(() {
+            //print(value);
+/*             for (int i = 0; i < value.length; i++) {
+              myRoute.add(value[i]);
+            } */
+            myRoute = value;
+          }));
+    }
+
     _getCurrentLocation();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: FlutterMap(
-      options: MapOptions(
-          minZoom: 7,
-          maxZoom: 18,
-          zoom: 8,
-          center: LatLng(46.5480234, 6.4022017),
-          maxBounds: LatLngBounds(
-              LatLng(45.398181, 5.140242), LatLng(48.230651, 11.47757))),
-      layers: [
-        TileLayerOptions(
-          urlTemplate:
-              'https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg',
-        ),
-        PolylineLayerOptions(
-          polylines: [
-            Polyline(
-              strokeWidth: 3,
-              color: Colors.black,
-              points: [
-                LatLng(46.3808464, 6.8059153),
-                LatLng(46.4174536, 7.1895558),
-                LatLng(46.1855684, 7.3205926),
+    return Stack(
+      children: [
+        Scaffold(
+            body: FlutterMap(
+          options: MapOptions(
+              onTap: (tapPosition, point) {
+                setState(() {
+                  _clicked = point;
+                });
+                print(point.toString());
+              },
+              minZoom: 9,
+              maxZoom: 18,
+              zoom: 9,
+              center: LatLng(46.5480234, 6.4022017),
+              maxBounds: LatLngBounds(
+                  LatLng(45.398181, 5.140242), LatLng(48.230651, 11.47757))),
+          layers: [
+            switchValue
+                ? TileLayerOptions(
+                    urlTemplate:
+                        'https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.strassenkarte-200/default/current/3857/{z}/{x}/{y}.png',
+                  )
+                : TileLayerOptions(
+                    urlTemplate:
+                        'https://wmts.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg',
+                  ),
+
+            PolylineLayerOptions(
+              polylines: [
+                Polyline(
+                  strokeWidth: 3,
+                  color: Colors.yellow,
+                  points: [...myRoute],
+                ),
               ],
             ),
+            MarkerLayerOptions(markers: [
+              //retrieve user position (if he accepts to give it) and display a bike on the map
+              Marker(
+                point: LatLng(_position == null ? 0 : _position!.latitude,
+                    _position == null ? 0 : _position!.longitude),
+                width: 80,
+                height: 80,
+                builder: (context) => Icon(
+                  Icons.place,
+                  color: Colors.red,
+                ),
+              ),
+
+              Marker(
+                point: _clicked,
+                width: 200,
+                height: 200,
+                builder: (context) => Icon(
+                  Icons.place,
+                  color: Colors.green,
+                ),
+              ),
+              if (myRoute.isNotEmpty)
+                Marker(
+                  point: myRoute.first,
+                  width: 80,
+                  height: 80,
+                  builder: (context) => Icon(Icons.directions_bike),
+                ),
+
+              //canvas/attempt to make start/end point
+
+              if (myRoute.isNotEmpty)
+                Marker(
+                  point: myRoute.last,
+                  width: 80,
+                  height: 80,
+                  builder: (context) => Icon(
+                    Icons.flag,
+                    color: Colors.green,
+                  ),
+                ),
+            ]),
+
+            // canvas/attempt to make a route
           ],
-        ),
-        MarkerLayerOptions(markers: [
-          //retrieve user position (if he accepts to give it) and display a bike on the map
-          Marker(
-            point: LatLng(_position == null ? 0 : _position!.latitude,
-                _position == null ? 0 : _position!.longitude),
-            width: 80,
-            height: 80,
-            builder: (context) => Icon(
-              Icons.place,
-              color: Colors.red,
-            ),
-          ),
-
-          //canvas/attempt to make start/end point
-          Marker(
-            point: LatLng(46.3808464, 6.8059153),
-            width: 80,
-            height: 80,
-            builder: (context) => Icon(Icons.directions_bike),
-          ),
-
-          Marker(
-            point: LatLng(46.1855684, 7.3205926),
-            width: 80,
-            height: 80,
-            builder: (context) => Icon(
-              Icons.flag,
-              color: Colors.green,
-            ),
-          ),
-        ]),
-
-        // canvas/attempt to make a route
+        )),
+        Switch(
+            value: switchValue,
+            onChanged: (val) {
+              setState(() {
+                switchValue = val;
+              });
+            })
       ],
-    ));
+    );
+  }
+
+  Future<List<LatLng>> _getRoute(List<LatLng> myRoute) async {
+    List<LatLng> myReturn = [];
+    if (myRoute == []) {
+      return myReturn;
+    }
+    List<String> myCoordinates =
+        myRoute.map((e) => '[ ${e.longitude} , ${e.latitude}]').toList();
+
+    print(myCoordinates.join(','));
+    var response = await http.post(
+        Uri.parse(
+            "https://api.openrouteservice.org/v2/directions/cycling-regular/geojson"),
+        headers: {
+          "Accept":
+              'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
+          "Content-Type": "application/json",
+          'Authorization':
+              '5b3ce3597851110001cf6248a744e0e2eadf4594b08f961125b1131d'
+        },
+        body: '{"coordinates":[ ${myCoordinates.join(',')}]}',
+        encoding: Encoding.getByName("utf-8"));
+
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      var jsonParsed = jsonDecode(response.body);
+      var feature = jsonParsed['features']!;
+
+      var coordinates = feature[0]['geometry']!['coordinates'];
+
+      for (int i = 0; i < coordinates.length; i++) {
+        myReturn.add(LatLng(coordinates[i][1], coordinates[i][0]));
+      }
+    }
+    return myReturn;
   }
 }
